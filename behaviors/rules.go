6ed527fedd532e47acb8d3520da1cf3f2f448782
Diego-All <dposadallano@hotmail.com>
2024-07-19 18:14:02 -0500

    func CreateFilesBelowDev()

--- behaviors/rules.go
+++ behaviors/rules.go
@@ -1,6 +1,9 @@
 package behaviors
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func ShowMenu() {
 	fmt.Println("Seleccione una opción:")
@@ -26,27 +29,110 @@ func ShowMenu() {
 
 	switch choice {
 	case 1:
-		Regla1()
+		CreateFilesBelowDev()
 	case 2:
-		Regla2()
+		ReadSSHInformation()
 	case 3:
-		Regla3()
+		LaunchIngressRemoteFileCopyToolsInContainer()
+	case 4:
+		OutboundConnectionToC2Servers()
+	case 5:
+		LaunchRemoteFileCopyToolsInContainer()
+	case 6:
+		DirectoryTraversalMonitoredFileRead()
+	case 7:
+		LinuxKernelModuleInjection()
+	case 8:
+		ClearLogActivities()
+	case 9:
+		RemoveBulkDatafromDisk()
+	case 10:
+		DetectCryptoMinersUsingTheStratumProtocol()
+	case 11:
+		LaunchSuspiciousNetworkToolInContainer()
+	case 12:
+		NetcatRemoteCodeExecutionInContainer()
 	default:
 		fmt.Println("Opción no válida")
 	}
 }
 
-func Regla1() {
-	fmt.Println("Ejecutando Regla1")
-	// Lógica para Regla1
+func CreateFilesBelowDev() {
+	fmt.Println("Ejecutando: Create files below dev")
+
+	filePath := "/dev/randomrtk"
+
+	// Crear el archivo
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println("Error al crear el archivo:", err)
+		return
+	}
+	defer file.Close()
+
+	// Escribir en el archivo
+	content := "Este es un rootkit de ejemplo en /dev/randomrtk"
+	_, err = file.WriteString(content)
+	if err != nil {
+		fmt.Println("Error al escribir en el archivo:", err)
+		return
+	}
+
+	fmt.Println("Archivo creado y contenido escrito con éxito")
+
 }
 
-func Regla2() {
-	fmt.Println("Ejecutando Regla2")
+func ReadSSHInformation() {
+	fmt.Println("Ejecutando: Read ssh information")
 	// Lógica para Regla2
 }
 
-func Regla3() {
+func LaunchIngressRemoteFileCopyToolsInContainer() {
 	fmt.Println("Ejecutando Regla3")
+	// Lógica para Regla3
+}
+
+func OutboundConnectionToC2Servers() {
+	fmt.Println("Ejecutando: Outbound Connection to C2 Servers")
+	// Lógica para Regla3
+}
+
+func LaunchRemoteFileCopyToolsInContainer() {
+	fmt.Println("Ejecutando: Launch Remote File Copy Tools in Container")
+	// Lógica para Regla3
+}
+
+func DirectoryTraversalMonitoredFileRead() {
+	fmt.Println("Ejecutando: Directory Traversal monitored file read")
+	// Lógica para Regla3
+}
+
+func LinuxKernelModuleInjection() {
+	fmt.Println("Ejecutando: Linux Kernel Module Injection")
+	// Lógica para Regla3
+}
+
+func ClearLogActivities() {
+	fmt.Println("Ejecutando: Clear Log Activities")
+	// Lógica para Regla3
+}
+
+func RemoveBulkDatafromDisk() {
+	fmt.Println("Ejecutando: Remove Bulk Data from Disk")
+	// Lógica para Regla3
+}
+
+func DetectCryptoMinersUsingTheStratumProtocol() {
+	fmt.Println("Ejecutando: Detect crypto miners using the Stratum protocol")
+	// Lógica para Regla3
+}
+
+func LaunchSuspiciousNetworkToolInContainer() {
+	fmt.Println("Ejecutando: Remove Bulk Data from Disk")
+	// Lógica para Regla3
+}
+
+func NetcatRemoteCodeExecutionInContainer() {
+	fmt.Println("Ejecutando: Detect crypto miners using the Stratum protocol")
 	// Lógica para Regla3
 }
